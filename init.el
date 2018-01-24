e6fdec7bf34d845b6f527fe5c7d4854af487233b
walder <walderye_online@126.com>
2018-01-24 11:02:06 +0800

    init.el更正默认查询目录函数

--- init.el
+++ init.el
@@ -1,17 +1,11 @@
 ;; Init.el
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
-
+(toggle-truncate-lines t)
 ;; 将打开init.el绑定到F2上
 (defun open-init-file ()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 (global-set-key (kbd "<f2>") 'open-init-file)
-;; 将笔记本绑定到F4上
 
 ;; 把目录lisp/添加到搜索路径中
 (add-to-list
@@ -20,6 +14,10 @@
   "lisp"
   user-emacs-directory))
 
+
+
+;; 设置初始查找目录
+(setq command-line-default-directory "~/Desktop/")
 ;; 每个require的feature对应一个lisp/下的一个elisp文件
 
 ;; 字体
